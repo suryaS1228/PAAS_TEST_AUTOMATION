ad3d9588d4d145177150f8c0ab75405b30a6d551
Sharath-Solartis <sharath_n@solartis.net>
2018-01-11 15:12:09 +0530

    poi copysheet added

--- Service_Testing/ServiceTesting_program/src/main/java/com/solartis/test/util/common/ExcelOperationsPOI.java
+++ Service_Testing/ServiceTesting_program/src/main/java/com/solartis/test/util/common/ExcelOperationsPOI.java
@@ -9,12 +9,16 @@ import java.nio.channels.FileChannel;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
 import org.apache.poi.hssf.usermodel.HSSFDateUtil;
 import org.apache.poi.hssf.usermodel.HSSFFormulaEvaluator;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.DateUtil;
 import org.apache.poi.ss.usermodel.FormulaEvaluator;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Workbook;
 
 import com.solartis.test.exception.POIException;
@@ -392,6 +396,62 @@ public class ExcelOperationsPOI
 		int sheetindex =this.workbook.getSheetIndex(this.worksheet);
 		this.workbook.setSheetName(sheetindex, newsheetnamee);
 		
+	}
+	
+	@SuppressWarnings("deprecation")
+	public void copysheet(String fromSheet, String toSheet)
+	{
+		CellStyle newStyle = this.workbook.createCellStyle(); // Need this to copy over styles from old sheet to new sheet. Next step will be processed below
+		Row row;
+		Cell cell;
+		HSSFSheet sheetFromOldWB = (HSSFSheet) this.workbook.getSheet(fromSheet);
+		HSSFSheet sheetForNewWB = (HSSFSheet) this.workbook.getSheet(toSheet);
+		for (int rowIndex = 0; rowIndex < sheetFromOldWB.getPhysicalNumberOfRows(); rowIndex++) 
+		{
+		    row = sheetForNewWB.createRow(rowIndex); //create row in this new sheet
+		    for (int colIndex = 0; colIndex < sheetFromOldWB.getRow(rowIndex).getPhysicalNumberOfCells(); colIndex++) 
+		    {
+		        cell = row.createCell(colIndex); //create cell in this row of this new sheet
+		        Cell c = sheetFromOldWB.getRow(rowIndex).getCell(colIndex, Row.CREATE_NULL_AS_BLANK ); //get cell from old/original WB's sheet and when cell is null, return it as blank cells. And Blank cell will be returned as Blank cells. That will not change.
+		        if (c.getCellType() == Cell.CELL_TYPE_BLANK)
+		        {
+		                    //System.out.println("This is BLANK " +  ((HSSFCell) c).getReference());
+		        }
+		        else 
+		        {  //Below is where all the copying is happening. First It copies the styles of each cell and then it copies the content.              
+		            CellStyle origStyle = c.getCellStyle();
+		            newStyle.cloneStyleFrom(origStyle);
+		            cell.setCellStyle(newStyle);    
+		            switch (c.getCellTypeEnum()) 
+		            {
+		                case STRING:                            
+		                   cell.setCellValue(c.getRichStringCellValue().getString());
+		                   break;
+		                case NUMERIC:
+		                    if (DateUtil.isCellDateFormatted(cell)) 
+		                    {                             
+		                        cell.setCellValue(c.getDateCellValue());
+		                    } 
+		                    else 
+		                    {                              
+		                        cell.setCellValue(c.getNumericCellValue());
+		                    }
+		                    break;
+		               case BOOLEAN:
+		                  cell.setCellValue(c.getBooleanCellValue());
+		                  break;
+		               case FORMULA:
+		                  cell.setCellValue(c.getCellFormula());
+		                  break;
+		               case BLANK:
+		                  cell.setCellValue("who");
+		                  break;
+		               default:
+		               System.out.println();
+		            }
+		        }
+		    }
+		}		    
 	}
 	
 }
