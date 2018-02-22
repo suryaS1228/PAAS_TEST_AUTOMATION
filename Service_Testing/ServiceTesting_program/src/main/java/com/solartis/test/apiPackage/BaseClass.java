f443b342e01dd735cc564cea1a59199f7d42aa77
Sharath-Solartis <sharath_n@solartis.net>
2018-02-22 12:06:53 +0530

    commit-02222018

--- Service_Testing/ServiceTesting_program/src/main/java/com/solartis/test/apiPackage/BaseClass.java
+++ Service_Testing/ServiceTesting_program/src/main/java/com/solartis/test/apiPackage/BaseClass.java
@@ -330,7 +330,7 @@ public class BaseClass
 		//DatabaseOperation.ConnectionSetup("com.mysql.jdbc.Driver","jdbc:mysql://192.168.84.225:3700/Starr_DTC_Development_ADMIN","root","redhat");
 		table1=db.GetDataObjects("SELECT AnalyserResult, COUNT(*) as NoOfCount FROM OUTPUT_DTC_Rating_SinglePlan  GROUP BY AnalyserResult");
 		Iterator<Entry<Integer, LinkedHashMap<String,String>>> inputtableiterator = table1.entrySet().iterator();
-		excelreportlocation=config.getProperty("report_location")+"ExcelReport"+DateandTime+".xls";
+		excelreportlocation=config.getProperty("report_location")+"AnalysisReport"+DateandTime+".xls";
 		 ExcelOperationsPOI ob=new ExcelOperationsPOI(config.getProperty("report_template_location")+"ResultTemplate.xls");
 		 ob.getsheets("TestReport");
 		 ob.write_data(5, 4,config.getProperty("Project")+"-"+config.getProperty("Api"));
