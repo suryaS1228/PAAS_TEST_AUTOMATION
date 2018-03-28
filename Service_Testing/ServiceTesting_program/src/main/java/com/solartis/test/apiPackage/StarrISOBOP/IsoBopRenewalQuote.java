45bf1d28faa7dbf8e46c92199037ee0baaa040f2
Sharath-Solartis <sharath_n@solartis.net>
2018-03-28 12:44:41 +0530

    RenewalQuote

---
+++ Service_Testing/ServiceTesting_program/src/main/java/com/solartis/test/apiPackage/StarrISOBOP/IsoBopRenewalQuote.java
@@ -0,0 +1,21 @@
+package com.solartis.test.apiPackage.StarrISOBOP;
+
+import java.sql.SQLException;
+import java.util.LinkedHashMap;
+
+import com.solartis.test.Configuration.PropertiesHandle;
+import com.solartis.test.apiPackage.API;
+import com.solartis.test.apiPackage.BaseClass;
+import com.solartis.test.util.api.DBColoumnVerify;
+
+public class IsoBopRenewalQuote extends BaseClass implements API 
+{
+	public IsoBopRenewalQuote(PropertiesHandle config) throws SQLException
+	{
+		this.config = config;
+		jsonElements = new LinkedHashMap<String, String>();
+		InputColVerify = new DBColoumnVerify(config.getProperty("InputCondColumn"));
+		OutputColVerify = new DBColoumnVerify(config.getProperty("OutputCondColumn"));	
+		StatusColVerify = new DBColoumnVerify(config.getProperty("OutputCondColumn"));
+	}
+}
