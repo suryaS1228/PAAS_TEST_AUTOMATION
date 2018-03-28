bd11d1b7109f89809e506b1c2a47f9302d687556
Sharath-Solartis <sharath_n@solartis.net>
2018-03-28 17:25:53 +0530

    logs

--- Service_Testing/ServiceTesting_program/src/main/java/com/solartis/test/listener/LogListener.java
+++ Service_Testing/ServiceTesting_program/src/main/java/com/solartis/test/listener/LogListener.java
@@ -18,112 +18,112 @@ public class LogListener implements Listener
 	@Override
 	public void beforeLoadSampleRequest()
 	{
-		System.out.println("beforeLoadSampleRequest");
-		Log.info("beforeLoadSampleRequest");
+		//System.out.println("beforeLoadSampleRequest");
+		Log.debug("beforeLoadSampleRequest");
 	}
 
 	@Override
 	public void afterLoadSampleRequest() 
 	{
-		System.out.println("afterLoadSampleRequest");
-		Log.info("afterLoadSampleRequest");
+		//System.out.println("afterLoadSampleRequest");
+		Log.debug("afterLoadSampleRequest");
 	}
 
 	@Override
 	public void beforePumpDataToRequest()
 	{
-		System.out.println("beforePumpDataToRequest");
-		Log.info("beforePumpDataToRequest");
+		//System.out.println("beforePumpDataToRequest");
+		Log.debug("beforePumpDataToRequest");
 	}
 
 	@Override
 	public void afterPumpDataToRequest() 
 	{
-		System.out.println("afterPumpDataToRequest");
-		Log.info("afterPumpDataToRequest");
+		//System.out.println("afterPumpDataToRequest");
+		Log.debug("afterPumpDataToRequest");
 	}
 	@Override
 	public void beforeRequestToString()
 	{
-		System.out.println("beforeRequestToString");
-		Log.info("beforeRequestToString");
+		//System.out.println("beforeRequestToString");
+		Log.debug("beforeRequestToString");
 	}
 
 	@Override
 	public void afterRequestToString() 
 	{
-		System.out.println("afterRequestToString");
-		Log.info("afterRequestToString");
+		//System.out.println("afterRequestToString");
+		Log.debug("afterRequestToString");
 	}
 
 	@Override
 	public void beforeAddHeaders()
 	{
-		System.out.println("beforeAddHeaders");
-		Log.info("beforeAddHeaders");
+		//System.out.println("beforeAddHeaders");
+		Log.debug("beforeAddHeaders");
 	}
 
 	@Override
 	public void afterAddHeaders() 
 	{
-		System.out.println("afterAddHeaders");
-		Log.info("afterAddHeaders");
+		//System.out.println("afterAddHeaders");
+		Log.debug("afterAddHeaders");
 	}
 
 	@Override
 	public void beforeSendAndReceiveData()
 	{
-		System.out.println("beforeSendAndReceiveData");
-		Log.info("beforeSendAndReceiveData");
+		//System.out.println("beforeSendAndReceiveData");
+		Log.debug("beforeSendAndReceiveData");
 	}
 
 	@Override
 	public void afterSendAndReceiveData() 
 	{
-		System.out.println("afterSendAndReceiveData");
-		Log.info("afterSendAndReceiveData");
+		//System.out.println("afterSendAndReceiveData");
+		Log.debug("afterSendAndReceiveData");
 	}
 	
 	@Override
 	public void beforeResponseToString()
 	{
-		System.out.println("beforeResponseToString");
-		Log.info("beforeResponseToString");
+		//System.out.println("beforeResponseToString");
+		Log.debug("beforeResponseToString");
 	}
 
 	@Override
 	public void afterResponseToString() 
 	{
-		System.out.println("afterResponseToString");
-		Log.info("afterResponseToString");
+		//System.out.println("afterResponseToString");
+		Log.debug("afterResponseToString");
 	}
 
 	@Override
 	public void beforeSendResponseDataToFile()
 	{
-		System.out.println("beforeSendResponseDataToFile");
-		Log.info("beforeSendResponseDataToFile");
+		//System.out.println("beforeSendResponseDataToFile");
+		Log.debug("beforeSendResponseDataToFile");
 	}
 
 	@Override
 	public void afterSendResponseDataToFile() 
 	{
-		System.out.println("afterSendResponseDataToFile");
-		Log.info("afterSendResponseDataToFile");
+		//System.out.println("afterSendResponseDataToFile");
+		Log.debug("afterSendResponseDataToFile");
 	}
 
 	@Override
 	public void beforeCompareFunction() 
 	{
-		System.out.println("beforeCompareFunction");
-		Log.info("beforeCompareFunction");
+		//System.out.println("beforeCompareFunction");
+		Log.debug("beforeCompareFunction");
 	}
 
 	@Override
 	public void afterCompareFunction() 
 	{
-		System.out.println("afterCompareFunction");
-		Log.info("afterCompareFunction");
+		//System.out.println("afterCompareFunction");
+		Log.debug("afterCompareFunction");
 	}
 
 	@Override
