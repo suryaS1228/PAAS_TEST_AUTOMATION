133ab90fdefae7a82b17a90a589ad53d4dc43cb2
suganya <suganya_tk@solartis.com>
2018-06-25 19:07:26 +0530

    Adding Event version to the header.

--- Service_Testing/ServiceTesting_program/src/main/java/com/solartis/test/apiPackage/UKLondonBOP/UKLondonBOPPayIssue.java
+++ Service_Testing/ServiceTesting_program/src/main/java/com/solartis/test/apiPackage/UKLondonBOP/UKLondonBOPPayIssue.java
@@ -32,6 +32,7 @@ public class UKLondonBOPPayIssue extends BaseClass implements API
 			http.AddHeader("Content-Type", config.getProperty("content_type"));
 			http.AddHeader("Token", Token);
 			http.AddHeader("EventName", config.getProperty("EventName"));
+			http.AddHeader("EventVersion", config.getProperty("EventVersion"));
 		}
 		catch (HTTPHandleException e) 
 		{
