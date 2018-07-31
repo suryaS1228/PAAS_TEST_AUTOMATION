47070868771ef42a6fafdfa8c7231b02b7a18baa
suganya <suganya_tk@solartis.com>
2018-07-31 13:12:52 +0530

    Embroker
    Java file and FTL file added for quote API.

---
+++ Service_Testing/ServiceTesting_program/src/main/java/com/solartis/test/apiPackage/Embroker/EmbrokerQuote.java
@@ -0,0 +1,43 @@
+package com.solartis.test.apiPackage.Embroker;
+
+import java.util.LinkedHashMap;
+import com.solartis.test.Configuration.PropertiesHandle;
+import com.solartis.test.apiPackage.API;
+import com.solartis.test.apiPackage.BaseClass;
+import com.solartis.test.exception.APIException;
+import com.solartis.test.exception.HTTPHandleException;
+import com.solartis.test.util.api.DBColoumnVerify;
+import com.solartis.test.util.api.HttpHandle;
+
+public class EmbrokerQuote extends BaseClass implements API
+{
+	public EmbrokerQuote(PropertiesHandle config)
+	{
+		this.config = config;
+		jsonElements = new LinkedHashMap<String, String>();
+		
+		InputColVerify = new DBColoumnVerify(config.getProperty("InputCondColumn"));
+		OutputColVerify = new DBColoumnVerify(config.getProperty("OutputCondColumn"));	
+		StatusColVerify = new DBColoumnVerify(config.getProperty("OutputCondColumn"));	
+	}
+
+	@Override
+	public void AddHeaders(String Token) throws APIException
+	{
+		try 
+		{
+			http = new HttpHandle(config.getProperty("test_url"),"POST");
+			http.AddHeader("Content-Type", config.getProperty("content_type"));
+			http.AddHeader("Token", Token);
+			http.AddHeader("EventName", config.getProperty("EventName"));
+			http.AddHeader("EventVersion", config.getProperty("EventVersion"));
+		}
+		catch (HTTPHandleException e) 
+		{
+			e.printStackTrace();
+			throw new APIException("ERROR ADD HEADER FUNCTION -- STARSEARCH&RESCUE-ISSUECERTIFICATE CLASS", e);
+		}
+	}
+	 
+	
+}
