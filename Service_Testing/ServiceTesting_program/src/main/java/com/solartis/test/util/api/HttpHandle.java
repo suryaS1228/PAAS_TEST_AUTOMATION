d8a0810ab1d93c9be17520c058fae8099e8997cd
sasirekha <sasirekha_a@solartis.com>
2018-08-03 17:10:52 +0530

    commit-08032018
    changes in httphandler

--- Service_Testing/ServiceTesting_program/src/main/java/com/solartis/test/util/api/HttpHandle.java
+++ Service_Testing/ServiceTesting_program/src/main/java/com/solartis/test/util/api/HttpHandle.java
@@ -8,6 +8,16 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import com.solartis.test.exception.HTTPHandleException;
 
@@ -21,6 +31,8 @@ public class HttpHandle
 		
 			try 
 			{
+				disableSslVerification();
+
 				this.url = new URL(url);
 			} 
 			catch (MalformedURLException e) 
@@ -53,7 +65,6 @@ public class HttpHandle
 	public void AddHeader(String attr,String value)
 	{
 		conn.setRequestProperty(attr, value);
-		
 	}
 	
 	public void SendData(String data) throws HTTPHandleException
@@ -62,6 +73,7 @@ public class HttpHandle
 		
 			try 
 			{
+				
 				os = conn.getOutputStream();
 				os.write(data.getBytes());
 				os.flush();
@@ -115,5 +127,43 @@ public class HttpHandle
 		
 		return output2;
 	}
-	
+	public static void disableSslVerification() {
+        try
+        {
+            // Create a trust manager that does not validate certificate chains
+            TrustManager[] trustAllCerts = new TrustManager[] {new X509TrustManager() {
+                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+                @Override
+                public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                }
+                @Override
+                public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                }
+				
+                
+            }
+            };
+            // Install the all-trusting trust manager
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+            // Create all-trusting host name verifier
+            HostnameVerifier allHostsValid = new HostnameVerifier() {
+                @Override
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
+                }
+
+			
+            };
+            // Install the all-trusting host verifier
+            HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+        }
+}
 }
