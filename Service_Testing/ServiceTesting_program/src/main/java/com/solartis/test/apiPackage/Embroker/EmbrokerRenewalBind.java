2ca8678d7ae78fa76b1d8864590420cd25a126d3
sarath <sarathchandar_e@solartis.com>
2019-07-24 15:07:23 +0530

    Embroker
    RenewalBind, RenewalQuote and Runoff and return

---
+++ Service_Testing/ServiceTesting_program/src/main/java/com/solartis/test/apiPackage/Embroker/EmbrokerRenewalBind.java
@@ -0,0 +1,130 @@
+package com.solartis.test.apiPackage.Embroker;
+
+import java.util.LinkedHashMap;
+import java.util.Map.Entry;
+
+import com.jayway.jsonpath.PathNotFoundException;
+import com.solartis.test.Configuration.PropertiesHandle;
+import com.solartis.test.apiPackage.API;
+import com.solartis.test.apiPackage.BaseClass;
+import com.solartis.test.exception.APIException;
+import com.solartis.test.exception.DatabaseException;
+import com.solartis.test.exception.HTTPHandleException;
+import com.solartis.test.exception.MacroException;
+import com.solartis.test.exception.POIException;
+import com.solartis.test.exception.RequestFormatException;
+import com.solartis.test.util.api.DBColoumnVerify;
+import com.solartis.test.util.api.HttpHandle;
+import com.solartis.test.util.api.JsonHandle;
+
+public class EmbrokerRenewalBind extends BaseClass implements API
+{
+	public EmbrokerRenewalBind(PropertiesHandle config)
+	{
+		this.config = config;
+		jsonElements = new LinkedHashMap<String, String>();
+		
+		InputColVerify = new DBColoumnVerify(config.getProperty("InputCondColumn"));
+		OutputColVerify = new DBColoumnVerify(config.getProperty("OutputCondColumn"));	
+		StatusColVerify = new DBColoumnVerify(config.getProperty("OutputCondColumn"));	
+	}
+
+	public String tokenGenerator(PropertiesHandle config)
+	{
+		
+		String Token="";
+		try
+		{
+			System.out.println(config.getProperty("AuthenticationURL"));
+			HttpHandle http = new HttpHandle(config.getProperty("AuthenticationURL"),"POST");
+			http.AddHeader("Content-Type", config.getProperty("content_type"));
+			String input_data = "{  \"ServiceRequestDetail\": { \"OwnerId\": \""+config.getProperty("OwnerID")+"\", \"ResponseType\": \"JSON\", \"BrowserIp\": \"192.168.5.140\", \"ServiceRequestVersion\": \"2.0\" }, \"UserCredential\": { \"UserName\": \""+config.getProperty("Userneme")+"\",    \"Password\": \""+config.getProperty("Password")+"\"  } }";
+			http.SendData(input_data);
+			String response_string = http.ReceiveData();	
+			System.out.println(input_data+"/n/n/n"+response_string);
+			JsonHandle response = new JsonHandle();
+			//response.StringToFile(response_string);
+			//response.FileToString();
+			Token = Token+response.readToken("$..Token",response_string).replaceAll("\\[\"", "").replaceAll("\"\\]", "").replaceAll("\\\\","");
+			System.out.println(Token);
+		}
+		catch(Exception e)
+		{
+			System.out.println("Error in Generating Token");
+			e.printStackTrace();
+		}
+		return Token;
+	}
+
+	@Override
+	public void AddHeaders(String Token) throws APIException
+	{
+		try 
+		{
+			http = new HttpHandle(config.getProperty("test_url"),"POST");
+			http.AddHeader("Content-Type", config.getProperty("content_type"));
+			http.AddHeader("Token", Token);
+			http.AddHeader("EventName", config.getProperty("EventName"));
+			http.AddHeader("EventVersion", config.getProperty("EventVersion"));
+		}
+		catch (HTTPHandleException e) 
+		{
+			e.printStackTrace();
+			throw new APIException("ERROR ADD HEADER FUNCTION -- STARSEARCH&RESCUE-ISSUECERTIFICATE CLASS", e);
+		}
+	}
+	 @Override
+	 public LinkedHashMap<String, String> SendResponseDataToFile(LinkedHashMap<String, String> output)   throws APIException
+	 {
+		try
+		{
+			 if(config.getProperty("Execution_Flag").equals("ActualOnly")||config.getProperty("Execution_Flag").equals("Comparison")||config.getProperty("Execution_Flag").equals("ActualandComparison"))
+			 {
+			LinkedHashMap<Integer, LinkedHashMap<String, String>> tableOutputColVerify = OutputColVerify.GetDataObjects(config.getProperty("OutputColQuery"));
+			
+			String ResponseStatus=response.read("..ResponseStatus").replaceAll("\\[\"", "").replaceAll("\"\\]", "").replaceAll("\\\\","");
+			if(ResponseStatus.equals("SUCCESS"))
+			{
+			
+			for (Entry<Integer, LinkedHashMap<String, String>> entry : tableOutputColVerify.entrySet())	
+			{
+				LinkedHashMap<String, String> rowOutputColVerify = entry.getValue();
+				  if((rowOutputColVerify.get("Flag").equalsIgnoreCase("Y"))&&conditioncheck.ConditionReading(rowOutputColVerify.get("OutputColumnCondtn"),input))
+					{
+					try
+						{
+					
+						String actual = (response.read(rowOutputColVerify.get(config.getProperty("OutputJsonPath"))).replaceAll("\\[\"", "")).replaceAll("\"\\]", "").replaceAll("\\\\","");
+		
+						output.put(rowOutputColVerify.get(config.getProperty("OutputColumn")), actual);
+						output.put("Flag_for_execution", ResponseStatus);
+						}
+						catch(PathNotFoundException | RequestFormatException e)
+						{
+							output.put(rowOutputColVerify.get(config.getProperty("OutputColumn")), "Path not Found");
+						}
+					}
+			}
+			}
+			else
+			{
+				output.put("Flag_for_execution", "FailedResponse");
+				
+				String Message=response.read("..Message").replaceAll("\\[\"", "").replaceAll("\"\\]", "").replaceAll("\\\\","");
+				String Message2=response.read("..UserMessage").replaceAll("\\[\"", "").replaceAll("\"\\]", "").replaceAll("\\\\","");
+				output.put("User_message",Message);
+				output.put("User_message2",Message2);
+				output.put("AnalyserResult","Rule-"+Message2);
+				
+			}
+		 }
+			
+		}
+		catch(DatabaseException | RequestFormatException e)
+		{
+			 throw new APIException("ERROR SendResponseDataToFile FUNCTION -- DTC-RatingService CLASS", e);
+		}
+		return output;
+	}
+	
+}
